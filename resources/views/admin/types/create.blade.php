f84d2429e0974965a53ea11625baab5ad97b3b67
danilopiccinni <danilopiccinni@gmail.com>
2023-05-25 07:12:17 +0200

    style forms types

--- resources/views/admin/types/create.blade.php
+++ resources/views/admin/types/create.blade.php
@@ -4,11 +4,13 @@
 
     <div class="container px-5 my-5" >
 
+        <h2 class="mb-5 text-center">Creazione nuova tipologia di progetto</h2>
+
         <form action="{{ route('admin.types.store') }}" method="POST">
     
             @csrf
 
-            <div class="mb-2 form-check" >
+            <div class="mb-5 form-check" >
                 <label class="form-label" for="name">Nome:</label>
                 <input class="form-control @error('name') is-invalid @enderror" type="text" id="name" name="name" value="{{old('name')}}">
                 @error('name')
@@ -18,9 +20,9 @@
                 @enderror
             </div>
     
-            <div class="mb-2 form-check" >
+            <div class="mb-5 form-check" >
                 <label class="form-label" for="description">Descrizione:</label>
-                <textarea class="form-control @error('description') is-invalid @enderror" type="text" id="description" name="description">{{old('description')}}</textarea>
+                <textarea style="height: 150px" class="form-control @error('description') is-invalid @enderror" type="text" id="description" name="description">{{old('description')}}</textarea>
                 @error('description')
                 <div class="invalid-feedback">
                     <em> {{$message}} </em>
@@ -28,7 +30,7 @@
                 @enderror
             </div>
 
-            <button class="btn btn-secondary ms-4 mt-3" type="submit">Crea nuovo tipo progetto</button>
+            <button class="btn btn-secondary ms-4 mt-3" type="submit">Crea nuova tipologia di progetto</button>
      
         </form>
     </div>
--- resources/views/admin/types/edit.blade.php
+++ resources/views/admin/types/edit.blade.php
@@ -4,12 +4,14 @@
 
     <div class="container px-5 my-5" >
 
+        <h2 class="mb-5 text-center">Modifica tipologia di progetto</h2>
+
         <form action="{{ route('admin.types.update', $type) }}" method="POST">
     
             @csrf
             @method('PUT')
 
-            <div class="mb-2 form-check" >
+            <div class="mb-5 form-check" >
                 <label class="form-label" for="name">Nome:</label>
                 <input class="form-control @error('name') is-invalid @enderror" type="text" id="name" name="name" value="{{old('name') ?? $type->name}}">
                 @error('name')
@@ -19,9 +21,9 @@
                 @enderror
             </div>
     
-            <div class="mb-2 form-check" >
+            <div class="mb-5 form-check" >
                 <label class="form-label" for="description">Descrizione:</label>
-                <textarea class="form-control @error('description') is-invalid @enderror" type="text" id="description" name="description">{{old('description') ?? $type->name}}</textarea>
+                <textarea style="height: 150px" class="form-control @error('description') is-invalid @enderror" type="text" id="description" name="description">{{old('description') ?? $type->name}}</textarea>
                 @error('description')
                 <div class="invalid-feedback">
                     <em> {{$message}} </em>
@@ -29,7 +31,7 @@
                 @enderror
             </div>
 
-            <button class="btn btn-secondary ms-4 mt-3" type="submit">Attua modifiche</button>
+            <button class="btn btn-secondary ms-4 mt-3" type="submit">Modifica tipologia</button>
      
         </form>
     </div>
