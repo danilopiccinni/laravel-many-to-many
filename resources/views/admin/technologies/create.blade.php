5255929645a0faa2d32a1cdb65faec000df1a73f
danilopiccinni <danilopiccinni@gmail.com>
2023-05-25 07:07:54 +0200

    style forms technologies

--- resources/views/admin/technologies/create.blade.php
+++ resources/views/admin/technologies/create.blade.php
@@ -4,11 +4,13 @@
 
     <div class="container px-5 my-5" >
 
+        <h2 class="mb-5 text-center">Creazione nuova tecnologia</h2>
+
         <form action="{{ route('admin.technologies.store') }}" method="POST">
     
             @csrf
 
-            <div class="mb-2 form-check" >
+            <div class="mb-5 form-check" >
                 <label class="form-label" for="name">Nome:</label>
                 <input class="form-control @error('name') is-invalid @enderror" type="text" id="name" name="name" value="{{old('name')}}">
                 @error('name')
@@ -18,9 +20,9 @@
                 @enderror
             </div>
     
-            <div class="mb-2 form-check" >
+            <div class="mb-5 form-check" >
                 <label class="form-label" for="description">Descrizione:</label>
-                <textarea class="form-control @error('description') is-invalid @enderror" type="text" id="description" name="description">{{old('description')}}</textarea>
+                <textarea style="height: 150px" class="form-control @error('description') is-invalid @enderror" type="text" id="description" name="description">{{old('description')}}</textarea>
                 @error('description')
                 <div class="invalid-feedback">
                     <em> {{$message}} </em>
@@ -28,9 +30,9 @@
                 @enderror
             </div>
 
-            <div class="mb-2 form-check" >
-                <label class="form-label" for="color">colore:</label>
-                <input class="form-control @error('color') is-invalid @enderror" type="color" id="color" name="color" value="{{old('name')}}">
+            <div class="mb-5 form-check d-flex justify-content-center align-items-center gap-3" >
+                <label class="form-label" for="color">Colore sfondo badge:</label>
+                <input style="width: 100px; height: 50px" class="form-control @error('color') is-invalid @enderror" type="color" id="color" name="color" value="{{old('name')}}">
                 @error('color')
                 <div class="invalid-feedback">
                     <em> {{$message}} </em>
@@ -38,7 +40,7 @@
                 @enderror
             </div>
 
-            <button class="btn btn-secondary ms-4 mt-3" type="submit">Crea nuovo tipo progetto</button>
+            <button class="btn btn-secondary ms-4 mt-3" type="submit">Crea nuova tecnologia</button>
      
         </form>
     </div>
--- resources/views/admin/technologies/edit.blade.php
+++ resources/views/admin/technologies/edit.blade.php
@@ -4,12 +4,14 @@
 
     <div class="container px-5 my-5" >
 
+        <h2 class="mb-5 text-center">Creazione nuova tecnologia</h2>
+
         <form action="{{ route('admin.technologies.update' ,$technology) }}" method="POST">
     
             @csrf
             @method('PUT')
 
-            <div class="mb-2 form-check" >
+            <div class="mb-5 form-check" >
                 <label class="form-label" for="name">Nome:</label>
                 <input class="form-control @error('name') is-invalid @enderror" type="text" id="name" name="name" value="{{old('name') ?? $technology->name}}">
                 @error('name')
@@ -19,9 +21,9 @@
                 @enderror
             </div>
     
-            <div class="mb-2 form-check" >
+            <div class="mb-5 form-check" >
                 <label class="form-label" for="description">Descrizione:</label>
-                <textarea class="form-control @error('description') is-invalid @enderror" type="text" id="description" name="description">{{old('description') ?? $technology->description}}</textarea>
+                <textarea style="height: 150px" class="form-control @error('description') is-invalid @enderror" type="text" id="description" name="description">{{old('description') ?? $technology->description}}</textarea>
                 @error('description')
                 <div class="invalid-feedback">
                     <em> {{$message}} </em>
@@ -29,9 +31,9 @@
                 @enderror
             </div>
 
-            <div class="mb-2 form-check" >
+            <div class="mb-5 form-check d-flex justify-content-center align-items-center gap-" >
                 <label class="form-label" for="color">colore:</label>
-                <input class="form-control @error('color') is-invalid @enderror" type="color" id="color" name="color" value="{{old('name') ?? $technology->color}}">
+                <input style="width: 100px; height: 50px" class="form-control @error('color') is-invalid @enderror" type="color" id="color" name="color" value="{{old('name') ?? $technology->color}}">
                 @error('color')
                 <div class="invalid-feedback">
                     <em> {{$message}} </em>
@@ -39,7 +41,7 @@
                 @enderror
             </div>
 
-            <button class="btn btn-secondary ms-4 mt-3" type="submit">Modifica</button>
+            <button class="btn btn-secondary ms-4 mt-3" type="submit">Modifica tecnologia</button>
      
         </form>
     </div>
